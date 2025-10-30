13b149d136a3473efffc133b12a3a10c8302a4e6
Linus <Linus.sideback03@gmail.com>
2025-10-30 11:07:48 +0100

    First

---
+++ src/main/java/com/example/D0031N/D0031NApplication.java
@@ -0,0 +1,13 @@
+package com.example.D0031N;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D0031NApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D0031NApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/D0031N/D0031NApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.D0031N;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D0031NApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
